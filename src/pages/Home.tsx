c0b7ea2090166223d2f2f4df0ff9366b36ee96b3
vuvuvo0303 <levanhoangvu2003@gmail.com>
2024-05-17 15:41:26 +0700

    Initial commit

---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import { Input } from "antd";
+
+const Home = () => {
+  return (
+    <div className="h-[100vh] flex flex-col justify-center">
+      <h1 className="text-6xl bg-red-500 text-white p-3 text-center">Hello wolrd</h1>
+      <Input />
+    </div>
+  );
+};
+
+export default Home;
