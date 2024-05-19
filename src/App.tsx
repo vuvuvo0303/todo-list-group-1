fa299190ae31bcebe749b021cf9b76a7b52d9bde
duongtoan106 <phamduongtoan106@gmail.com>
2024-05-19 20:15:45 +0700

    delete react

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
-import React from "react";
 import Home from "./pages/Home";
 import { ToastContainer } from "react-toastify";
-import 'react-toastify/dist/ReactToastify.css';
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
